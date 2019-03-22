d1a7fd2daddf7ffb5993025b74de61d7890515c9
Sajantoor98 <Sajantoor98@gmail.com>
2019-03-21 18:22:47 -0700

    done

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-let num = "1111";
-let deci = 0;
 
- decimal(num);
+// checks if the number is a valid binary number
+function checkBinaryValue() {
+  value = document.getElementById('binaryInput').value;
+  for (var i = 0; i < value.length; ) {
+    let x = value[i];
+    if (x != 0 && x != 1)  { // Checks if the dits contain anything other than 1 or a 0.
+      alert('Invalid value');
+      return;
+    }
+    i++
+  }
+  decimal(value); // pushes to function for computation to binary to decimal
+}
 
+// Binary to decimal function
 function decimal(num) {
+  let deci = 0;
   for (var i = 0; i < num.length;) {
     // x is the current digit
     let x = parseInt(num[num.length - i - 1]);
@@ -15,6 +27,39 @@ function decimal(num) {
     }
     i++;
   }
+  document.getElementById('BinaryToDecimal').innerText = deci;
 }
 
-console.log(deci);
+// Checks whether or not it's a valid number
+function checkNumValue() {
+  value = document.getElementById('decimalInput').value;
+  for (var i = 0; i < value.length;) {
+    let x = value[i];
+    if (isNaN(x)) { // Checks if it's isn't a number and returns
+      console.log('not a number')
+      return;
+    }
+    i++
+  }
+  binary(value);
+}
+
+// Decimal to binary function
+function binary(num) {
+  // initializes the string and array. 
+  var binNum = "";
+  var binaryVal = [];
+
+  while (num > 0) {
+    var digit = num % 2; // gets remainder, it's either 1 or 0.
+    num = Math.trunc(num / 2); // gets rid of the decimal, effectively making it an interger like C++
+    binaryVal.push(digit); // pushes to the array;
+  }
+
+  // Reverse the binary digits to proper order.
+  for (var i = (binaryVal.length - 1); i >= 0; i--) {
+    binNum = binNum + binaryVal[i];
+  }
+
+  document.getElementById('DecimalToBinary').innerText = binNum;
+}
